f9605c60b75ef1f2013d994ba08313cea859f492
elina-wang <siaosyuan0611@gmail.com>
2020-06-21 16:23:02 +0800

    finish

---
+++ js/slick.js
@@ -0,0 +1,17 @@
+AOS.init();
+
+$('#banner').slick({
+    autoplay:true,
+    autoplaySpeed:1000,
+    arrows:false,
+
+});
+
+
+$('#icon').slick({
+    autoplay:true,
+    autoplaySpeed:1000,
+    slidesToShow:4,
+    dots: true,
+    arrows:false
+});
